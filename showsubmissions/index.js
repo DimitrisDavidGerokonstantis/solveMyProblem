b828659f13df0284d5a2245b96f0fc1bed5c76a6
ntua-el19209 <el19209@mail.ntua.gr>
2024-04-24 21:47:25 +0300

    consumers start when app starts running - fixed a bug

--- showsubmissions/index.js
+++ showsubmissions/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import routes from "./src/routes/routes.js";
 import cors from "cors";
 import mongoose from "mongoose"
+import { consume_from_questions_queue } from "./src/consumeQuestion.js";
+
 
 
 mongoose.connect(process.env.MONGO_DB_URI);
@@ -19,6 +21,12 @@ app.use(cors(corsOptions));
 
 app.use("/api", routes);
 
-app.listen(5000, () => {
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(() => resolve(), ms));
+}
+
+app.listen(5000, async() => {
   console.log("Connected!!!");
+  await sleep(10000);
+  consume_from_questions_queue();
 });
