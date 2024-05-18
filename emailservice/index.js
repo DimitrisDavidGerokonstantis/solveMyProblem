a79dbd744977084b044affd53a525256b862b665
ntua-el19209 <el19209@mail.ntua.gr>
2024-05-18 17:11:13 +0300

    added email service
    Welcome new users, notify for produced answers ...

--- emailservice/index.js
+++ emailservice/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import routes from "./src/routes/routes.js";
-import googleRoutes from "./src/routes/oauth.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
@@ -11,20 +10,14 @@ mongoose.connect(process.env.MONGO_DB_URI);
 const app = express();
 
 const corsOptions = {
-  origin: [
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "http://submitproblem:5000",
-    "http://showsubmissions:5000",
-  ],
+  origin: ["http://localhost:8080", "http://localhost:3000"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/auth", routes);
-app.use("/googleAuth", googleRoutes);
+app.use("/email", routes);
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(() => resolve(), ms));
@@ -32,6 +25,6 @@ function sleep(ms) {
 
 app.listen(5000, async () => {
   console.log("Connected!!");
-  await sleep(20000);
+  await sleep(30000);
   consume_from_answers_queue();
 });
